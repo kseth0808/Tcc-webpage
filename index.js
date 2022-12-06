5ae7890849eb2f0b5f79f7cba2512ad19f25c50f
Kushagra <kseth0808@gmail.com>
2022-12-06 22:32:27 +0530

    edit

--- index.js
+++ index.js
@@ -13,23 +13,6 @@ window.onload = function () {
        document.getElementById("header").style.backgroundImage="url('"+backgroundImg[i]+"')";
      }
    }
-
-  //  window.onload = function () {
-  //   // Array of Images
-  //    var backgroundImgs=["./imgs2/img5.jpg",
-  //                      "./imgs2/img6.jpg",
-  //                      "./imgs2/img7.jpg",
-  //                      "./imgs2/img8.jpg"
-  //                      ]
-
-  //      setInterval(changeImages, 2000);
-  //     function changeImages() {   
-  //      var a = Math.floor((Math.random() * 3));
-
-  //      document.getElementById("image").style.backgroundImage="url('"+backgroundImgs[a]+"')";
-  //    }
-  //  }
-   
    var navlinks = document.getElementById("navlinks");
     function showMenu() {
       navlinks.style.right = "0"
@@ -37,46 +20,36 @@ window.onload = function () {
     function hideMenu() {
       navlinks.style.right = "-200px"
     }
-
-
-    // var swiper = new Swiper(".mySwiper", {
-    //   navigation: {
-    //     nextEl: ".swiper-button-next",
-    //     prevEl: ".swiper-button-prev",
-    //   },
-    //   loop:true,
-    //   autoplay: {
-    //     delay: 500,
-    //     disableOnInteraction: false,
-    //   },
-    // });
-
-    // local reviews data
 const reviews = [
   {
     id: 1,
-    img: "https://media-exp1.licdn.com/dms/image/C4D22AQEjUqSwbGhbcA/feedshare-shrink_2048_1536/0/1652436911198?e=1671667200&v=beta&t=HaIKInEVzpULQmaQ0dn3yjGEnvd_j-03OBsyxkn84dA"
+    img: "./imgs/img6.jpg"
   },
   {
     id: 2,
     img:
-      "https://media-exp1.licdn.com/dms/image/C5622AQF61F786v8NJA/feedshare-shrink_800/0/1664017881288?e=1671667200&v=beta&t=F8Bd2QJGxiv0DpRhQIzaB4QSAFb7Dog2ynLDq2xlxjg"
+      "./imgs/img7.jpg"
   },
   {
     id: 3,
     img:
-      "https://media-exp1.licdn.com/dms/image/C4D22AQHx5XJRfuzUgw/feedshare-shrink_1280/0/1663513861535?e=1671667200&v=beta&t=9EXrqstldhWIWQ6VirmEVAu62OjYogIydZk4Q2KPBTg"
+      "./imgs/img8.jpg"
   },
   {
     id: 4,
     img:
-      "https://media-exp1.licdn.com/dms/image/C4D22AQHOW0Qwdsr7Vw/feedshare-shrink_1280/0/1663235085710?e=1671667200&v=beta&t=sr-eVL-oDTiSCPo2HYR9o43OIJnZ_8f7EJCMBOxLpO0"
+      "./imgs/img8.jpg"
   },
   {
     id: 5,
     img:
-      "https://media-exp1.licdn.com/dms/image/C4D22AQHGTtQRkJShOw/feedshare-shrink_1280/0/1657091363258?e=1671667200&v=beta&t=NrdyqWQ_19Ypc9USeiD3FUVP9dnCECd8pBSMU4mBhg0"
+      "./imgs/img9.jpg"
   },
+  {
+    id: 5,
+    img:
+      "./imgs/img12.jpg"
+  }
 ];
 
 const img= document.getElementById('person-img');
